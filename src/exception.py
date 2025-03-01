d0842b91cf75e7b15368878e1ef1e1101ac57fdd
Srilakshmi Kokanti <its.sriluu@gmail.com>
2025-03-01 11:04:23 -0600

    EDA and Modeling

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import
+from src.logger import logging
 def error_message_detail(error, error_detail:sys): # error_detail is a module
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -14,10 +14,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("division by zero")
-        raise CustomException(e, sys) from e
+     
